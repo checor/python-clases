c78f75e4448ee3063044fcdb3f4017d99ac653e6
Sergio Urbina <sergio.urbina@shoelovers.co>
2019-08-01 18:18:54 -0500

    Ej 1

---
+++ sesion04/reto01/hoteles_txt.py
@@ -0,0 +1,21 @@
+cont  = True
+
+while cont:
+    nombre = input("Inserte nombre del hotel: ")
+    ubicacion = input("Inserte ubicación: ")
+    precio = input("Inserte precio por noche: ")
+    with open("hoteles.txt", 'a') as hoteles_file:
+        hoteles_file.write("{}\t{}\t{}\n".format(nombre, ubicacion, precio))
+    valid = False
+    while not valid:
+        c = input("Agregar otro hotel (s/N)? ")
+        c = c.lower()
+        if c.startswith('s'):
+            cont = True
+            valid = True
+        elif c.startswith('n'):
+            cont = False
+            valid = True
+        else:
+            print("Respuesta no válida")
+            valid = False
--- sesion04/reto01/readme.md
+++ sesion04/reto01/readme.md
@@ -2,4 +2,8 @@
 
 `hoteles.txt`
 
-Pedir al usuario una lista, con nombre un hotel, ubicación, y precio por habitación. Guardar la información en un archivo llamado hoteles.txt. Anexar al final la información si ya existe el archivo.
+Pedir al usuario una lista, con nombre un hotel, ubicación, y precio por habitación. Guardar la información en un archivo llamado hoteles.txt, separado por tabulador. Anexar al final la información si ya existe el archivo.
+
+```
+
+```
